6e4a373f227da2fb23e2a127cd29d645102d7f72
DanielBaks <daniel_baks@hotmail.com>
2020-11-30 17:02:12 +0100

    first

---
+++ gui-tkinter.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from tkinter import *
+
+
+#fenetre
+
+window = Tk()
+
+#personaliser 
+
+window.title("my app")
+window.geometry("1024x768")
+window.minimize(480, 360)
+# afficher
+window.mainloop()
